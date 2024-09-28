d96f59b3528871adecdc86f19cc3485a9c766f9e
KristianJakCBS <krja22ac@student.cbs.dk>
2024-09-28 17:59:25 +0200

    fjernet firebaseconfig

--- App.js
+++ App.js
@@ -3,19 +3,9 @@ import { StyleSheet, Text, View } from 'react-native';
 import { getApps, initializeApp } from "firebase/app";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useState, useEffect } from 'react';
+import { firebaseConfig } from './firebaseConfigProd';
 import AuthScreen from './screens/AuthScreen';
 import MainScreen from './screens/MainScreen';
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCp07UZ5xGigHmSix2_-pM2cnDnbTsvcS0",
-  authDomain: "fir-auth-b0269.firebaseapp.com",
-  databaseURL: "https://fir-auth-b0269-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "fir-auth-b0269",
-  storageBucket: "fir-auth-b0269.appspot.com",
-  messagingSenderId: "531068124015",
-  appId: "1:531068124015:web:dedb41dcb29fa96879c69a"
-};
 
 export default function App() {
   const [user, setUser] = useState({ loggedIn: false });
---
+++ readme.md
@@ -0,0 +1,13 @@
+# WASHMATE
+## Beskrivelse
+Denne readMe fil beskriver hvordan du sætter denne applikation til at køre
+
+### installation
+Kopier repository
+kør 'npm install' i terminalen
+
+### firebase config
+Denne applikation bruger firebase. Du skal selv tilslutte din egen firebase-database.
+Sørg for at du har et firebase web-projekt, hvor sign-in ved brug af Email/Password er aktiveret.
+Lav en kopi af firebaseConfigTemplate.js kaldet firebaseConfigProd.js
+Indsæt din egen firebaseConfig 
